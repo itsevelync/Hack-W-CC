e48849f202e4672f0593cb00227cd87d1dfa5725
itsevelync <evelynchen555@gmail.com>
2025-03-02 09:43:59 -0500

    Actually imported tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
 
     return (
-        <div>
-            Navbar
-        </div>
+        <>
+            <div className="w-full bg-[#e8eaed] flex justify-between px-4 py-2">
+                <h1>Logo</h1>
+                <div className="flex gap-4">
+                    <Link to="/">Home</Link>
+                    <Link to="/">About</Link>
+                    <Link to="/">Team</Link>
+                </div>
+            </div>
+        </>
     )
 }
 
