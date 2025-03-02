b9ba704c60d47a408dfa739c93c20478d1d66a9c
Hebe3834 <hebeluna00@gmail.com>
2025-03-02 13:02:26 -0500

    small navbar change x3

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,15 +7,15 @@ function Navbar() {
     return (
         <>
             <div className="w-[1000px] max-width-[90%] flex justify-between items-center gap-10 flex-wrap mx-auto my-5">
-                <Link to="/about" className="flex flex-col content-center">
-                    <img className="w-[50px] mx-auto" src={Scroll} />
+                <Link to="/about" className="flex flex-col content-center gap-2  text-lg">
+                    <img className="h-[70px] mx-auto" src={Scroll} />
                     About Us
                 </Link>
                 <Link to="/home" className="flex content-center justify-center h-min">
-                    <h1 className="text-2xl">MESSAGE IN A BOTTLE</h1>
+                    <h1 className="text-2xl">- MESSAGE IN A BOTTLE -</h1>
                 </Link>
-                <Link to="/submit" className="flex flex-col content-center">
-                    <img className="w-[50px] mx-auto" src={Feather} />
+                <Link to="/submit" className="flex flex-col content-center gap-2 text-lg">
+                    <img className="h-[70px] mx-auto" src={Feather} />
                     Send a Bottle
                 </Link>
             </div>
