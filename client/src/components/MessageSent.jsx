4fe175a09ffc90f865c6a200f89f6d63996b0ab0
Hebe3834 <hebeluna00@gmail.com>
2025-03-02 15:15:36 -0500

    works?

--- client/src/components/MessageSent.jsx
+++ client/src/components/MessageSent.jsx
@@ -2,10 +2,11 @@ import Bottle from '../assets/Bottle.png';
 
 function MessageSent() {
     return (
-        <div className='flex flex-col m-8 items-center' style={{ 
-            animation: 'messageSent 3s ease-in-out forwards' 
+        <div className='flex flex-col m-8 relative items-center bg-m-beige w-full h-full message-sent' style={{ 
+            animation: 'messageSent 3s ease-in-out forwards', 
+            opacity: 1
         }}>
-            <img src={Bottle} width='300'></img>
+            <img src={Bottle} width='300' className=''></img>
             <text className='py-4'>Bottle tossed in the ocean!</text>
         </div>
     )
